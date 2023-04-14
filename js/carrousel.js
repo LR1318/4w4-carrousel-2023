5ebd83991e137e44a87da314177ff46277a45998
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-04-14 11:18:02 -0400

    s11 c2 lab5 debut

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,45 @@
 (function(){
-    console.log("Debut du carrousel");
+ console.log('Début du carrousel')
+let bouton = document.querySelector(".carrousel__ouvrir")
+/* -------------------------------------------------------- Variable du carrousel */
+let carrousel  = document.querySelector(".carrousel")
+let carrousel__x = document.querySelector(".carrousel__x")
+let carrousel__figure = document.querySelector(".carrousel__figure")
+
+/* -------------------------------------------------------- Variable de la galerie */
+let galerie = document.querySelector(".galerie")
+let galerie__img = galerie.querySelectorAll("img")
+// console.log("galerie__img: " + galerie__img.length)
+// console.log(carrousel.tagName)
+
+
+/* ----------------------------------------------------  ouvrir boîte modale */
+bouton.addEventListener('mousedown', function(){
+    console.log('ouvrir la boîte modale')
+    carrousel.classList.add('carrousel--activer')
+    ajouter_img_de_galerie()
+
+})
+/* ----------------------------------------------------  fermer boîte modale */
+carrousel__x.addEventListener('mousedown', function(){
+    console.log('fermer la boîte modale')
+    carrousel.classList.remove('carrousel--activer')
+
+})
+/** 
+ * ajouter_img_de_galerie
+ * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
+ */
+function ajouter_img_de_galerie()
+{
+  for (const elm of galerie__img)
+  {
+    //console.log(elm.getAttribute('src'))
+    let img = document.createElement('img')
+    img.setAttribute('src', elm.getAttribute('src'))
+    console.log (img.getAttribute('src'))
+    carrousel__figure.appendChild(img)
+  }
 }
-)()
+
+})()
